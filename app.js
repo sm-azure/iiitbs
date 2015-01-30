f5736150e663b4a448b0f45c3f011ef0c35f7538
EC2 Default User <ec2-user@ip-172-31-27-77.ap-southeast-1.compute.internal>
2015-01-30 17:22:08 +0000

    added ip address to response

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var ip = '', count = 0
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  ip = 'addr: '+add;
+})
 
 http.createServer(function(req, res){
 	res.writeHead(200);
-	res.end('Hello World\n');
+	var str = count++ + ')' + 'from IP:' + ip;
+	res.end(str);
 	console.log('Received a request\n');
 }).listen(3000);
 
